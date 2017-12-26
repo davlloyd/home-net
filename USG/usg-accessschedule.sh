a36e81f047354982b09b1fbcc6c370539f95714d
davlloyd <davidlloyd0@gmail.com>
2017-12-27 09:00:52 +1100

    Updated internet access times for kids group

--- USG/usg-accessschedule.sh
+++ USG/usg-accessschedule.sh
@@ -4,9 +4,9 @@ configure
 
 # Create Time period
 set service webproxy url-filtering squidguard time-period KidsBlock description 'Kids device off time'
-set service webproxy url-filtering squidguard time-period KidsBlock days all time 00:00-07:00,10:00-15:00
-set service webproxy url-filtering squidguard time-period KidsBlock days weekdays time 20:30-24:00
-set service webproxy url-filtering squidguard time-period KidsBlock days weekend time 22:00-24:00
+set service webproxy url-filtering squidguard time-period KidsBlock days all time 09:00-15:00
+#set service webproxy url-filtering squidguard time-period KidsBlock days weekdays time 20:30-24:00
+#set service webproxy url-filtering squidguard time-period KidsBlock days weekend time 22:00-24:00
 
 # Set address scope to match kids wifi VLAN 
 set service webproxy url-filtering squidguard source-group Kids address 192.168.200.0/24
@@ -17,3 +17,8 @@ set service webproxy url-filtering squidguard rule 1 time-period KidsBlock
 
 # Set access rules
 set service webproxy url-filtering squidguard rule 1 default-action block
+
+# Set listening adress
+set service webproxy listen-address 192.168.200.1
+
+commit
